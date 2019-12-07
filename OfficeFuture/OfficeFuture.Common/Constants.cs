905cf3a73e7a749cdd26e8fcdfd2e22021c65c66
Alexander_Bakushin <alexander_bakushin@epam.com>
2019-12-07 10:34:26 +0300

    добавлен stub проект asp net core

---
+++ OfficeFuture/OfficeFuture.Common/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OfficeFuture.Common
+{
+	public static class OfficeConstants
+	{
+		public const string HubName = "officefuturehub";
+	}
+}
